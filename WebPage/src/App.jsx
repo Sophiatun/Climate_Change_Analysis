f65196fc309f6d15b055e2b6da241362c516a010
Sophiatun <sophiazawtun@gmail.com>
2024-01-07 11:13:38 -0800

    update

---
+++ WebPage/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Home from "./components/Home";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import LineChart from "./components/LineChart";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [view, setView] = useState("home");
+
+  function button() {
+    return (
+      <button className="btn" >Hello</button>
+    )
+  }
+
+  return (
+    <>
+      <div className=" w-screen p-0">
+        <div className="flex w-full bg-neutral-300 font-bold h-16 p-5 gap-5">
+          <button className="text-center" onClick={() => setView("home")}>
+            Home
+          </button>
+          <button className="text-center" onClick={() => setView("linegraphs")}>
+            Visualizations
+          </button>
+        </div>
+        <div>{view == "home" && <Home></Home>}</div>
+        <div>{view == "linegraphs" && <LineChart></LineChart>}</div>
+        {button }
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ WebPage/src/components/LineChart.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef, useState } from "react";
+import Plotly from "plotly.js-dist-min";
+import * as d3 from "d3";
+
+function LineChart() {
+  const [plotData, setPlotData] = useState(null);
+  const [selected, setSelected] = useState("airPolution");
+  const [dropdown, setDropdown] = useState(false);
+  const plotRef = useRef(null);
+
+  const charts = {
+    
+    airPolution : {
+    location: "src/data/Global.csv",
+    title:"Average Land Temperature",
+    x: "Year",
+    y: ["LandAverageTemperature"],
+    type: "line",
+    description: "Test one",
+    },
+
+   cities : {
+    location: "src/data/AirPollution.csv",
+    title: "",
+    x: "Country",
+    y: ['South Africa'],
+    type: "line",
+    description: "test two",
+   },
+
+   countries: {
+    location: "src/data/Countries.csv",
+    title: "",
+    x: "Year",
+    y: [],
+    type: "",
+    description: "test two",
+   }
+
+
+
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await d3.csv(charts[selected].location);
+        return processData(data);
+      } catch (error) {
+        console.error("Error fetching or processing data:", error);
+        return null;
+      }
+    }
+
+    function processData(allRows) {
+      var x = [], y = [];
+
+      allRows.forEach(row => {
+        x.push(row[charts[selected].x]);
+        y.push(row[charts[selected].y]);
+      });
+
+      return { x, y };
+    }
+
+    fetchData().then(processedData => {
+      if (processedData) {
+        makePlotly(processedData);
+      }
+    });
+  }, [selected]);
+
+  function makePlotly({ x, y }) {
+    var traces = [{ x, y }];
+    setPlotData(traces);
+  }
+
+  useEffect(() => {
+    if (plotRef.current && plotData) {
+      Plotly.newPlot(plotRef.current, plotData);
+    }
+  }, [plotData]); // Dependency on plotData to re-render plot when data changes
+
+  return (
+<div className="fixed left-0 top-0 h-full w-full p-10 pt-24">
+  <button id="dropdownDefaultButton" onClick={() => setDropdown(!dropdown)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Select graph <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+  </svg>
+  </button>
+
+  {dropdown && 
+  <div id="dropdown" className="z-10 fixed top-30 left-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+        <li>
+          <a href="#" onClick={() => setSelected("airPolution")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Air Polution</a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setSelected("cities")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cities</a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setSelected("countries")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Countries</a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setSelected("global")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Global</a>
+        </li>
+      </ul>
+  </div>}
+<div className="relative w-full h-1/2" ref={plotRef}></div>
+<div>{charts[selected].description}</div>
+</div>)
+}
+
+export default LineChart;
